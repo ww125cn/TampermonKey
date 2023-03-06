4f8ef55604d3e32a1307a546e21898a180dabab3
ww125cn <124808729+ww125cn@users.noreply.github.com>
2023-03-06 23:55:50 +0800

    Update MilkTeaToolBox.js

--- MilkTeaToolBox.js
+++ MilkTeaToolBox.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         奶茶工具箱
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        *://*/*
